0a806d009c07af3c293b91d8a6eed5fdea47f276
Karine Ribeiro <karine.yasmin@outlook.com>
2025-04-26 15:53:48 -0300

    Implement CRUD endpoints for Categoria model in CatalogoApi

--- CatalogoApi/Models/Categoria.cs
+++ CatalogoApi/Models/Categoria.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 namespace CatalogoApi.Models;
 
 public class Categoria
-{
+{   
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
--- CatalogoApi/Program.cs
+++ CatalogoApi/Program.cs
@@ -21,11 +21,54 @@ app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 });
 
+app.MapGet("/categorias", async (AppDbContext db) =>
+    await db.Categorias.ToListAsync());
+
+app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    return await db.Categorias.FindAsync(id)
+        is Categoria categoria  
+            ?Results.Ok(categoria)
+            :Results.NotFound();
+});
+
+app.MapPut("/categorias/{id:int}", async (int id, Categoria categoria, AppDbContext db) => 
+{
+    if (categoria.CategoriaId != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var categoriaDB = await db.Categorias.FindAsync(id);
+
+    if (categoriaDB is null) return Results.NotFound();
+
+    categoriaDB.Nome = categoria.Nome;
+    categoriaDB.Descricao = categoria.Descricao;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(categoriaDB);
+});
+
+app.MapDelete("/categorias/{id:int}", async(int id, AppDbContext db) =>
+{
+    var categoria = await db.Categorias.FindAsync(id);
+
+    if (categoria is null) 
+    {
+        return Results.NotFound();
+    }
+
+    db.Categorias.Remove(categoria);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.Run();
