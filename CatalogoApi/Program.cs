e3f7775e35585ab5032d1e917592c069bce449a0
Karine Ribeiro <karine.yasmin@outlook.com>
2025-04-26 13:21:52 -0300

    Initialize CatalogoApi project with Entity Framework Core, PostgreSQL, and basic structure

---
+++ CatalogoApi/Program.cs
@@ -0,0 +1,26 @@
+using CatalogoApi.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
